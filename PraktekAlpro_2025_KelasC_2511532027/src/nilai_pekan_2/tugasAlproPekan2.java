cb727b0ec2e989400b2794e10f8fc7b9b0014391
panasonic <panasonic@DESKTOP-AV5V6NT>
2025-09-19 23:59:05 +0700

    Tugas Alpro Pekan 2

---
+++ PraktekAlpro_2025_KelasC_2511532027/src/nilai_pekan_2/tugasAlproPekan2.java
@@ -0,0 +1,23 @@
+package nilai_pekan_2;
+
+public class tugasAlproPekan2 {
+	
+	static int jumlahpyrox=72000;
+	public static void main(String[] args) {
+		int Level = 90;
+		float Harga = 50.99f;
+		char Stats = 'S';
+		int JumlahChar = 200;
+		boolean Sepuh = true;
+		
+		System.out.println("*** Detail Akun Game ***");
+		System.out.println("Level        : " + Level);
+		System.out.println("Harga        : $" + Harga);
+		System.out.println("Stats        : " + Stats);
+		System.out.println("Jumlah Char  : " + JumlahChar);
+		System.out.println("Jumlah Pyrox : " + jumlahpyrox);
+		System.out.println("Sepuh        : " + Sepuh);
+
+	}
+
+}
