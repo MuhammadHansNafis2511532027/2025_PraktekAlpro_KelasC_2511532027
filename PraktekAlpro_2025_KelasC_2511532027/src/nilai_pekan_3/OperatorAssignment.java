288cdce8f3f768f5f97f8677f8e735317e9560b7
MuhammadHansNafis2511532027 <2511532027_muhammad@student.unand.ac.id>
2025-09-23 11:55:55 +0700

    Praktek Alpro Pekan 3

---
+++ PraktekAlpro_2025_KelasC_2511532027/src/nilai_pekan_3/OperatorAssignment.java
@@ -0,0 +1,36 @@
+package nilai_pekan_3;
+
+import java.util.Scanner;
+
+public class OperatorAssignment {
+
+	public static void main(String[] args) {
+		int A1;
+		int A2;
+		Scanner keyboard = new Scanner (System.in);
+		
+		System.out.println("Input angka-1: ");
+		A1 = keyboard.nextInt(); //input 10
+		System.out.println("Input angka-2: ");
+		A2 = keyboard.nextInt(); //input 5
+		keyboard.close(); //input 5
+		
+		System.out.println("Assignment Penambahan");
+		A1 += A2; //penambahan, sekarang A1 = 15
+		System.out.println("Penambahan : " + A1);
+		System.out.println("Assignment Pengurangan");
+		A1 -= A2; // pengurangan, sekarang A1 = 10
+		System.out.println("Pengurangan : " + A1);
+		System.out.println("Assignment Perkalian");
+		A1 *= A2; // perkalian, sekarang A1 = 50
+		System.out.println("Perkalian : " + A1);
+		System.out.println("Assignment Hasil Bagi");
+		A1 /= A2; //hasil bagi, sekarang A1 = 10
+		System.out.println("Pembagian : " + A1);
+		System.out.println("Assignment Sisa Bagi");
+		A1 %= A2; // sisa bagi, sekarang A1 = 0
+		System.out.println("Sisa Bagi : " + A1);
+
+	}
+
+}
